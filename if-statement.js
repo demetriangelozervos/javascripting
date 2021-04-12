1e72dd991887570d875a58942d32d1f7200e74f8
demetriangelozervos <zervosde@msu.edu>
2021-04-12 18:32:30 -0400

    javascripting assignment

---
+++ if-statement.js
@@ -0,0 +1,20 @@
+// let fruit = 'orange'
+
+// if (fruit.length > 5) {
+//     console.log('The fruit name has more than five characters.')
+// } else {
+//     console.log('The fruit name has five characters or less.');
+// }
+
+
+const fruit = 'orange'
+
+if (fruit.length > 5) {
+    console.log('The fruit name has more than five characters.')
+} else {
+    console.log('The fruit name has five characters or less.');
+}
+
+
+
+
---
+++ rounding-numbers.js
@@ -0,0 +1,11 @@
+// let roundUp = 1.5
+// Math.round(1.5)
+// let rounded = Math.round(1.5)
+
+// console.log(rounded);
+
+
+const roundUp = 1.5
+const rounded = Math.round(roundUp)
+
+console.log(rounded)
